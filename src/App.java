fe4ff39d4bc777ca0e8fb505c0d20ed0557700f5
losikt <losik.tomas@gmail.com>
2023-04-17 17:35:47 +0200

    menu SUIIIIIIIIII

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int volba;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print(
+            "[1]   Přidání filmu\n"+
+            "[2]   Upravení filmu\n"+
+            "[3]   Smazání filmu\n"+
+            "[4]   Přidání hodnocení filmu\n"+
+            "[5]   Výpis filmů\n"+
+            "[6]   Vyhledání filmu\n"+
+            "[7]   Výpis herců/animátorů kteří se podíleli na více dílech\n"+
+            "[8]   Výpis filmů podle herců/animátorů\n"+
+            "[9]   Uložení informací o filmu\n"+
+            "[10]  Přidání filmu souborem\n"+
+            "[0] Konec\n"+
+            "Vyber akci: ");
+            volba = sc.nextInt();
+        }while(volba !=0);
     }
 }
